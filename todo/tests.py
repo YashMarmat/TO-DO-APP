aa337b1568f24aaab27a957a605a670f7b0f0ed6
Yash Marmat <yashmarmat08@gmail.com>
2020-04-05 18:39:21 +0530

    Update tests.py

--- todo/tests.py
+++ todo/tests.py
@@ -1,44 +1,24 @@
 
 from django.test import TestCase
-from django.http import HttpRequest
 from django.urls import reverse
 from todo.models import Schedule
-from todo.views import CreateView, UpdateView, DeleteView
 
 
-class TestUrls(TestCase):
-
-    def test_home_page_status_code(self): # homepage contains the list of topics or schedules
-        response = self.client.get(reverse('home'))
-        self.assertEquals(response.status_code, 200)
-
-    def test_schedule_create_status_code(self):
-        response = self.client.get(reverse('schedule_create'))
-        self.assertEquals(response.status_code, 200)
-
-    def test_home_page_uses_correct_template(self):
-        response = self.client.get(reverse('home'))
-        self.assertEquals(response.status_code, 200)
-        self.assertTemplateUsed(response, 'home.html')
-
-    def test_create_page_uses_correct_templates(self):
-        response = self.client.get(reverse('schedule_create'))
-        self.assertEquals(response.status_code, 200)
-        self.assertTemplateUsed(response, 'schedule_create.html')
-
-
-class TestSchedule(TestCase):
+class ScheduleTest(TestCase):
 
     def setUp(self):  # created a new schedule or topic
         self.post = Schedule.objects.create(
             topic = 'any topic'
             )
 
-    def test_content_in_topic(self):
+    def test_todo_model_content(self):
+        self.assertEqual(f'{self.post.topic}', 'any topic')
+
+    def test_todo_model_content_using_id(self):
         data = Schedule.objects.get(id=1)
         expected_result = f'{data.topic}'
         self.assertEquals(expected_result, 'any topic')
-
+    
     # Note: there is no detail page in this project thats why i didnt test that
 
     def test_schedule_update_view(self): # new
@@ -48,12 +28,11 @@ class TestSchedule(TestCase):
         self.assertEqual(response.status_code, 302)
 
     def test_post_delete_view(self): # new
-        response = self.client.get(
-        reverse('schedule_delete', args='1'))
-        self.assertEqual(response.status_code, 200)
+        response = self.client.post(reverse('schedule_delete', args='1'))
+        self.assertEqual(response.status_code, 302)
 
 
 # Note: 302
 # This status occurs when a resource or page you're attempting
 # to load has been temporarily moved to a different location
-
+    
